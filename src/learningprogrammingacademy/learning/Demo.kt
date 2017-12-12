15f46622b625004a3cdca26294893fe26ba1471f
Standley Eugene <estand25@gmail.com>
2017-12-11 20:33:23 -0500

    Section 2 Lecture 10 - Learn how to use for and foreach loops Lecture 11 - Loop Challenge Lecture 12 - Break and Continue Statements Lecture 13 - Using Strings

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -7,24 +7,43 @@ Standley Eugene
 */
 
 fun main(args: Array<String>){
-    val animals = arrayOf("Dog", "Cat", "Lizard", "Bird", "Snake")
 
-    for(animal in animals){
-        if(animal == "Lizard"){
-            break
-        }
+    val dog = "Dog"
+    val dog2 = String(charArrayOf('D','o','g')) // creates new reference of type string
 
-        println(animal)
-    }
-    // break jump to this line
+    println("dog === dog2 => " + (dog === dog2)) // reference equality (point to same point in memory)
+    println("dog == dog2 => " + (dog == dog2)) // structural equality (checks the contain of the string)  Same characters ad length
 
-    println("continue")
+    val cat = "Cat"
+    val cat2 = String(cat.toCharArray())
+    val cat3 = cat2 // reference to same object
 
-    for(animal in animals){
-        if(animal == "Lizard"){
-            continue
-        }
+    println("reference equal => " + (cat === cat2))
+    println("reference equal => " + (cat === cat3))
+    println("reference equal => " + (cat2 === cat2))
 
-        println(animal)
-    }
+    // templates
+    println("dog= $dog cat= $cat")
+
+    // raw
+    println(""" this is
+        my dog
+        $dog""")
+
+    // methods from String class
+    var lizard = "Lizard"
+
+    println("lizard first letter= ${lizard[0]}")
+    println("contian= ${lizard.contains("is")}")
+
+    // immutability
+    lizard.toUpperCase()
+
+    println("lizard= $lizard")
+
+    lizard = lizard.toUpperCase() // assigned new value to variable
+
+    println("lizard= $lizard")
+
+
 }
