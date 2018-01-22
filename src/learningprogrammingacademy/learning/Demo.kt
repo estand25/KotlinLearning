8b3158a33f680c3605bd107fa9ff8944ea928f4c
Standley Eugene <estand25@gmail.com>
2018-01-22 17:45:20 -0500

    Section 2, Lecture 20 - Class Constructors

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -6,17 +6,40 @@ Standley Eugene
 12/8/2017
 */
 
-fun main(args: Array<String>){
-    showInfo()
-    showInfo("Jimmy")
-    showInfo("Anthony", 10)
-    showInfo(level = 5, name = "Timmy")
+class Dog constructor(private val color: String = "Black") {
 
-    showLevelAndXp(5,1000)
-    showLevelAndXp(xp = 10, level = 3)
-    showLevelAndXp(level = 10, xp = 5000)
+    init {
+        println("dog Initialized with color value = $color")
+    }
+
+    fun printColor() = println("color = $color")
 }
 
-private fun showInfo(name: String = "John", level: Int = 2) = println("name = $name level= $level")
+class Cat(private val name: String = "Tom", private val age: Int = 3){
+    /*private var age = 3
 
-private fun showLevelAndXp(level: Int, xp:Int) = println("level = $level xp= $xp")
+    constructor(name: String = "Tom", age: Int = 50) :this(name){
+        this.age = age
+    }*/
+
+    fun printInfo() = println("name = $name age = $age")
+}
+
+fun main(args: Array<String>){
+    val dog = Dog()
+    dog.printColor()
+
+    val dog2 = Dog("gray")
+    dog2.printColor()
+
+    val cat = Cat()
+    cat.printInfo()
+
+    val cat2 = Cat(name = "Jerry")
+    cat2.printInfo()
+
+    val cat3 = Cat(name = "Jimmy", age = 10)
+    cat3.printInfo()
+
+}
+
