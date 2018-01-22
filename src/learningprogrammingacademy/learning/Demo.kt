e2bbef6bf9205c47fdc0f51c5da0e1bf1defd092
Standley Eugene <estand25@gmail.com>
2018-01-22 18:03:37 -0500

    Section 2, Lecture 21 - Getters and Setters

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -6,40 +6,49 @@ Standley Eugene
 12/8/2017
 */
 
-class Dog constructor(private val color: String = "Black") {
+class Player {
+    private var health = 150
+    private var alive = true
 
-    init {
-        println("dog Initialized with color value = $color")
+    fun reduceHealth(amount: Int){
+        setHealth(health - amount)
     }
 
-    fun printColor() = println("color = $color")
-}
+    fun setHealth(value: Int){
+        var newHealth = value
 
-class Cat(private val name: String = "Tom", private val age: Int = 3){
-    /*private var age = 3
+        if(value <= 0){
+            newHealth = 0 // keep health at 0
+            alive = false
+        }
 
-    constructor(name: String = "Tom", age: Int = 50) :this(name){
-        this.age = age
-    }*/
+        health = newHealth
+    }
 
-    fun printInfo() = println("name = $name age = $age")
+    fun getHealth() = health
+
+    fun isAlive() = alive
+
+    fun respawn() {
+        if(!alive){
+            health = 150
+            alive = true
+        }
+    }
+
+    fun printInfo() = println("health = $health alive = $alive")
 }
 
 fun main(args: Array<String>){
-    val dog = Dog()
-    dog.printColor()
+    val player = Player()
 
-    val dog2 = Dog("gray")
-    dog2.printColor()
+    player.reduceHealth(100)
+    player.printInfo()
 
-    val cat = Cat()
-    cat.printInfo()
+    player.reduceHealth(100)
+    player.printInfo()
 
-    val cat2 = Cat(name = "Jerry")
-    cat2.printInfo()
-
-    val cat3 = Cat(name = "Jimmy", age = 10)
-    cat3.printInfo()
-
+    player.respawn()
+    player.printInfo()
 }
 
