bda752201220835361e8dc1ce87223dc1ec46f1d
Standley Eugene <estand25@gmail.com>
2018-01-23 08:18:47 -0500

    Section 2, Lecture 25 - Static Methods

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -5,60 +5,38 @@ learning programming academy.learning
 Standley Eugene
 12/8/2017
 */
+object StringUtilis {
+//    fun startWithUpperCase(value: String?): Boolean {
+//        if(value.isNullOrBlank()){
+//            return false
+//        }
+//
+//        return value!![0].isUpperCase()
 
-/**
- *  write class the represents person
- *
- *  person has 3 properties/field firtName(String), lastName(String), age (int)
- *  write setter/ set block for age to limit age between 0 and 100
- *  create readonly property fullName (firstName + lastName
- *
- *  in main create instance of person
- *
- *  print firtName, lastName, fullName and age
- *
- *  tip: create method in person class to print properties
- */
+//        return !(value.isNullOrBlank()) && value!![0].isUpperCase()
+//    }
 
-class Person (var firstName: String,var lastName: String, age: Int) {
-    var age: Int = 0
-        set(value) {
-            println("Setting Block")
-            var newAge = value
-
-            if(newAge < 0)
-                newAge = 0
-
-            if(newAge > 100)
-                newAge = 100
-
-            field = newAge
-        }
-
-    init {
-        this.age = age
-    }
-
-    val fullName: String
-        get() = firstName + " " + lastName
-
-    fun printInfo(){
-        println("Person firstName = $firstName, lastName = $lastName, fullName = $fullName , Age= $age")
-    }
+    fun startWithUpperCase(value: String?) : Boolean = !(value.isNullOrBlank()) && value!![0].isUpperCase()
 }
 
+fun String?.startWithUpperCase() : Boolean = !(this.isNullOrBlank()) && this!![0].isUpperCase()
+infix fun Int.multiplyBy(multiplier : Int) = this * multiplier
+
 fun main(args: Array<String>){
-    val john = Person("John", "Smith", 25)
-    john.printInfo()
 
-    val jimmy = Person("Jimmy", "Winter", 120)
-    jimmy.printInfo()
+    val myString = "Kotlin"
 
-    val anthony = Person("Anthony", "Bell", -10)
-    anthony.printInfo()
+    println("toLowerCase= ${myString.toLowerCase()}")
 
-    println("anthony firstNam= ${anthony.firstName}")
+    println("startWithUpper= ${StringUtilis.startWithUpperCase(myString)}")
+    println("startWithUpper= ${StringUtilis.startWithUpperCase(null)}")
+    println("startWithUpper= ${StringUtilis.startWithUpperCase("")}")
+    println("startWithUpper= ${StringUtilis.startWithUpperCase("kotlin")}")
 
+    println("startWithUpper= ${myString.startWithUpperCase()}")
 
+    println("1 * 5= ${1.multiplyBy(5)}")
+
+    println("1 * 5= ${1 multiplyBy 5}")
 }
 
