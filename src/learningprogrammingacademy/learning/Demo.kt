e27e30d031238109a7f859b7ed9de04a5a5b361c
Standley Eugene <estand25@gmail.com>
2018-01-23 10:18:11 -0500

    Section 2, Lecture 29 - Polymorphism

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -6,14 +6,40 @@ Standley Eugene
 12/8/2017
 */
 
-open class Enemy(var health: Int, var weapon: String){
+open class Enemy(health: Int, var weapon: String){
+    var health: Int = 0
+    set(value) {
+        field = value
+
+        if(value < 0){
+            field = 0
+        }
+    }
+
+    var damage: Int = 0
+
     init {
+        this.health = health
         println("Enemy init called")
     }
 
-    open fun attack(){
-      println("attacking with $weapon")
+    open fun attack(enemy: Enemy){
+      println("attacking $enemy with $weapon")
+        enemy.takeDamage(damage)
     }
+
+    fun takeDamage(damageToTake: Int){
+        health -= damageToTake
+    }
+//    open fun attack(pikeman: Pikeman){
+//        println("attacking with $weapon")
+//    }
+//
+//    open fun attack(archer: Archer){
+//        println("attacking with $weapon")
+//    }
+
+
 }
 
 class Pikeman(health: Int, var armor: Int) : Enemy(health, "Pike"){
@@ -27,29 +53,35 @@ class Archer(health: Int,var arrowCount: Int): Enemy(health,"bow"){
         println("Archer init called")
     }
 
-    override fun attack() {
+    override fun attack(enemy: Enemy) {
         if(arrowCount <= 0){
             println("No more arrows")
 
         }
         else{
-            super.attack()
+            super.attack(enemy)
             arrowCount--
             println("arrows left = $arrowCount")
         }
     }
 }
 
-fun main(args: Array<String>){
-    val pikeman = Pikeman(100, 100)
-    pikeman.attack()
+fun main(args: Array<String>) {
+    val pikeman: Enemy = Pikeman(100, 100)
+    pikeman.damage = 40
 
-    val archer = Archer(100,5)
-    archer.attack()
-    archer.attack()
-    archer.attack()
-    archer.attack()
-    archer.attack()
-    archer.attack()
+    val archer: Enemy = Archer(100, 5)
+    archer.damage = 60
+
+    pikeman.attack(archer)
+    println("Pikeman health= ${pikeman.health}")
+
+    archer.attack(pikeman)
+    println("Archer health= ${archer.health}")
+
+    pikeman.attack(archer)
+    println("Pikeman health= ${pikeman.health}")
+
+    archer.attack(pikeman)
+    println("Archer health= ${archer.health}")
 }
-
