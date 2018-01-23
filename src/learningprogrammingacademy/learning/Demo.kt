10e758131678a5c28dd1c63e53897858f5df1054
Standley Eugene <estand25@gmail.com>
2018-01-23 09:07:20 -0500

    Section 2, Lecture 26 - Static Methods Challenge - Pass (1/2)

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -5,38 +5,66 @@ learning programming academy.learning
 Standley Eugene
 12/8/2017
 */
-object StringUtilis {
-//    fun startWithUpperCase(value: String?): Boolean {
-//        if(value.isNullOrBlank()){
-//            return false
-//        }
-//
-//        return value!![0].isUpperCase()
 
-//        return !(value.isNullOrBlank()) && value!![0].isUpperCase()
-//    }
+/**
+ * extenstion
+ * write extension method that will return true if string has even number of characters
+ * or false if string has odd number of characters
+ * test method in main with few different string
+ *
+ * StringUtils
+ * write method in StringUtils that will calculate sum of two string lengths, or in others words sum of characters
+ * in 2 string
+ * tip: use modulus operator for even odd testing
+ * tip: be carful with null values and empty string
+ */
+object StringUtils {
+    fun totalStringLength(a: String?, b: String?): Int{
+        if(a.isNullOrEmpty() && b.isNullOrEmpty())
+            return 0
+        else if(a.isNullOrEmpty())
+            return b!!.length
+        else if(b.isNullOrBlank())
+            return a!!.length
 
-    fun startWithUpperCase(value: String?) : Boolean = !(value.isNullOrBlank()) && value!![0].isUpperCase()
+        return a!!.length + b!!.length
+    }
 }
 
-fun String?.startWithUpperCase() : Boolean = !(this.isNullOrBlank()) && this!![0].isUpperCase()
-infix fun Int.multiplyBy(multiplier : Int) = this * multiplier
+fun String?.isLengthEven() = !(this.isNullOrBlank()) && (this!!.length % 2 == 0)
 
 fun main(args: Array<String>){
 
-    val myString = "Kotlin"
+    var test: String? = "1234"
+    println("Test = ${test.isLengthEven()}")
 
-    println("toLowerCase= ${myString.toLowerCase()}")
+    test = "12345"
+    println("Test = ${test.isLengthEven()}")
 
-    println("startWithUpper= ${StringUtilis.startWithUpperCase(myString)}")
-    println("startWithUpper= ${StringUtilis.startWithUpperCase(null)}")
-    println("startWithUpper= ${StringUtilis.startWithUpperCase("")}")
-    println("startWithUpper= ${StringUtilis.startWithUpperCase("kotlin")}")
+    test = null
+    println("Test = ${test.isLengthEven()}")
 
-    println("startWithUpper= ${myString.startWithUpperCase()}")
+    test = ""
+    println("Test = ${test.isLengthEven()}")
 
-    println("1 * 5= ${1.multiplyBy(5)}")
+    var a: String? = "123"
+    var b: String? = "123"
+    println("sum length a & b = ${StringUtils.totalStringLength(a,b)}")
 
-    println("1 * 5= ${1 multiplyBy 5}")
+    a = ""
+    b = "123"
+    println("sum length a & b = ${StringUtils.totalStringLength(a,b)}")
+
+    a = "123"
+    b = null
+    println("sum length a & b = ${StringUtils.totalStringLength(a,b)}")
+
+    a = null
+    b = null
+    println("sum length a & b = ${StringUtils.totalStringLength(a,b)}")
+
+    a = ""
+    b = ""
+    println("sum length a & b = ${StringUtils.totalStringLength(a,b)}")
 }
 
