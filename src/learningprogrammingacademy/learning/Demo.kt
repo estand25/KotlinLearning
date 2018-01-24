33aa6bfad10d6e0a4e56dae1e9fcc4547fde70af
Standley Eugene <estand25@gmail.com>
2018-01-24 12:26:42 -0500

    Section 2, Lecture 37 - Understand Map

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -1,7 +1,5 @@
 package learningprogrammingacademy.learning
 
-import java.util.*
-
 /*
 learning programming academy.learning
 Standley Eugene
@@ -9,24 +7,39 @@ Standley Eugene
 */
 
 fun main(args: Array<String>) {
-    val deck = Stack<String>()
-    deck.push("Ace of diamonds") // Bottom
-    deck.push("Ace of hearts")
-    deck.push("Ace of clubs")
-    deck.push("Ace of spades") // Top
+//    val weaponsByDamage = hashMapOf<String,Int>()
+//    weaponsByDamage.put("pike", 10)
+//    weaponsByDamage.put("arrow", 15)
+//    weaponsByDamage.put("sword", 20)
+//    weaponsByDamage.put("pistol", 30)
 
-    println("size= ${deck.size}")
+    val weaponsByDamage = hashMapOf<String, Int>(
+            "pike" to 10,
+            "arrow" to 15,
+            "sword" to 20,
+            "pistol" to 30
+    )
 
-    for(card in deck){
-        println("card= $card")
+    for ((key,value) in weaponsByDamage){
+        println("key= $key value= $value")
     }
 
-    println("size= ${deck.size}")
-
-    while(deck.size > 0){
-        val cardName = deck.pop()
-        println("dealing card= $cardName")
+    for(key in weaponsByDamage){
+        println("key= $key")
     }
 
-    println("size= ${deck.size}")
+    for(value in weaponsByDamage){
+        println("value= $value")
+    }
+
+    //getting//setting values
+    println("sword has damage ${weaponsByDamage["sword"]}")
+
+    println("size= ${weaponsByDamage.size}")
+
+    weaponsByDamage["sword"] = 35
+
+    println("sword has new damage ${weaponsByDamage["sword"]}")
+
+    println("size= ${weaponsByDamage.size}")
 }
