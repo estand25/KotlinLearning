50dedbdd643389bc1a988604e835526dc472ae3c
Standley Eugene <estand25@gmail.com>
2018-01-24 12:48:37 -0500

    Section 2, Lecture 38 - Operator Overloading

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -6,40 +6,30 @@ Standley Eugene
 12/8/2017
 */
 
+class ComplexNumber(val real: Float, val imaginary: Float){
+    operator fun plus(other: ComplexNumber): ComplexNumber {
+        return ComplexNumber(real+ other.real, imaginary + other.imaginary)
+    }
+
+    override fun toString(): String {
+        var sign = "+"
+
+        if(imaginary < 0){
+            sign = "-"
+        }
+
+        return "$real $sign ${Math.abs(imaginary)}"
+    }
+}
+
 fun main(args: Array<String>) {
-//    val weaponsByDamage = hashMapOf<String,Int>()
-//    weaponsByDamage.put("pike", 10)
-//    weaponsByDamage.put("arrow", 15)
-//    weaponsByDamage.put("sword", 20)
-//    weaponsByDamage.put("pistol", 30)
+    val first = ComplexNumber(2f,2f)
+    val second = ComplexNumber(3f, -5f)
 
-    val weaponsByDamage = hashMapOf<String, Int>(
-            "pike" to 10,
-            "arrow" to 15,
-            "sword" to 20,
-            "pistol" to 30
-    )
+    println("first= $first")
+    println("second= $second")
 
-    for ((key,value) in weaponsByDamage){
-        println("key= $key value= $value")
-    }
+    val sum = first + second
 
-    for(key in weaponsByDamage){
-        println("key= $key")
-    }
-
-    for(value in weaponsByDamage){
-        println("value= $value")
-    }
-
-    //getting//setting values
-    println("sword has damage ${weaponsByDamage["sword"]}")
-
-    println("size= ${weaponsByDamage.size}")
-
-    weaponsByDamage["sword"] = 35
-
-    println("sword has new damage ${weaponsByDamage["sword"]}")
-
-    println("size= ${weaponsByDamage.size}")
+    println("sum= $sum")
 }
