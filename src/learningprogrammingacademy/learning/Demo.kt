b3c9216543baaef8b0e5fe19c978a1d6cf957fb4
Standley Eugene <estand25@gmail.com>
2018-01-24 14:31:18 -0500

    Section 2, Lecture 39 - Operator Overloading Challenge - (1/2)

--- src/learningprogrammingacademy/learning/Demo.kt
+++ src/learningprogrammingacademy/learning/Demo.kt
@@ -5,10 +5,25 @@ learning programming academy.learning
 Standley Eugene
 12/8/2017
 */
-
+/*
+    Using knowledge from previous lesson overload - (mius) and * (times) operator in Complex Number class
+    Print to console results of - and * operation
+ */
 class ComplexNumber(val real: Float, val imaginary: Float){
     operator fun plus(other: ComplexNumber): ComplexNumber {
         return ComplexNumber(real+ other.real, imaginary + other.imaginary)
+    }
+
+    operator fun minus(other: ComplexNumber): ComplexNumber{
+        return ComplexNumber(real - other.real, imaginary - other.imaginary)
+    }
+
+    operator fun times(other: ComplexNumber): ComplexNumber{
+        //return ComplexNumber(real * other.real, imaginary * other.imaginary)
+        val realResult = real  * other.real - imaginary * other.imaginary
+        val imaginaryResult = imaginary * other.real + real * other.imaginary
+
+        return ComplexNumber(realResult, imaginaryResult)
     }
 
     override fun toString(): String {
@@ -30,6 +45,10 @@ fun main(args: Array<String>) {
     println("second= $second")
 
     val sum = first + second
+    val minus = first - second
+    val multiply = first * second
 
     println("sum= $sum")
+    println("minus= $minus")
+    println("multiply= $multiply")
 }
